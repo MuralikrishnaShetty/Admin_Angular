e22b2613993c7bbb5f2651a1c1137b502df1781b
Muralikrishna <workformuralikrishnashetty@gmail.com>
2023-11-13 23:55:44 +0530

    CRUD on menu and USER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangeuserComponent } from './changeuser/changeuser.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { EdituserComponent } from './edituser/edituser.component';
+import { AdduserComponent } from './adduser/adduser.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+import { MenuslistComponent } from './menuslist/menuslist.component';
+import { EditmenuComponent } from './editmenu/editmenu.component';
+import { AddmenuComponent } from './addmenu/addmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangeuserComponent,
+    AdmindashboardComponent,
+    EdituserComponent,
+    AdduserComponent,
+    MenuslistComponent,
+    EditmenuComponent,
+    AddmenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
